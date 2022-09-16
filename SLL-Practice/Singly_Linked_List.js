1a247897b47da3427f11e111898c8c05deb4c127
dsabelli <dsabelli3@gmail.com>
2022-09-16 10:20:43 -0400

    push method

---
+++ SLL-Practice/Singly_Linked_List.js
@@ -0,0 +1,35 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const list = new SinglyLinkedList();
+
+list.push(69);
+list.push(60);
+list.push(22);
+list.push(30);
+list.push(44);
+console.log(list);
