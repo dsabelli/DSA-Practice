88feb6475ea0432e92ed27498c73573c0bbaba72
dsabelli <dsabelli3@gmail.com>
2022-09-19 09:04:08 -0400

    push method

--- DLL-Practice/Doubly_Linked_List.js
+++ DLL-Practice/Doubly_Linked_List.js
@@ -6,10 +6,32 @@ class Node {
   }
 }
 
-class SinglyLinkedList {
+class DoublyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    let node = new Node(val);
+    if (!this.length) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
 }
+
+const list = new DoublyLinkedList();
+
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+console.log(list.pop());
+console.log(list);
