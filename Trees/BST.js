cf69981e66905a5799bf0ceb2d52fb39c26bc7f8
dsabelli <dsabelli3@gmail.com>
2022-09-19 16:07:15 -0400

    find method

--- Trees/BST.js
+++ Trees/BST.js
@@ -33,6 +33,39 @@ class BinarySearchTree {
       }
     }
   }
+  find(val) {
+    if (!this.root) return false;
+    let current = this.root;
+    while (true) {
+      if (val === current.val) return true;
+      if (val < current.val) {
+        if (current.left === val) return true;
+        else if (current.left === null) return false;
+        current = current.left;
+      } else if (val > current.val) {
+        if (current.right === val) return true;
+        else if (current.right === null) return false;
+        current = current.right;
+      }
+    }
+  }
+  //   find(val) {
+  //     if (!this.root) return false;
+  //     let current = this.root;
+  //     let found = false;
+  //     while (current && !found) {
+  //       console.log(found);
+  //       if (val < current.val) {
+  //         current = current.left;
+  //       } else if (current > current.val) {
+  //         current = current.right;
+  //       } else {
+  //         found = true;
+  //       }
+  //     }
+  //     if (!found) return undefined;
+  //     return current;
+  //   }
 }
 
 const tree = new BinarySearchTree();
@@ -44,4 +77,4 @@ tree.insert(21);
 tree.insert(52);
 tree.insert(69);
 tree.insert(2);
-console.log(tree);
+console.log(tree.find(112));
