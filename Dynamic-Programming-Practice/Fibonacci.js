19f4755d4fff4c73b02dcd7dde5443d8215ff8da
dsabelli <dsabelli3@gmail.com>
2022-09-25 14:33:38 -0400

    use memoization

--- Dynamic-Programming-Practice/Fibonacci.js
+++ Dynamic-Programming-Practice/Fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = (n) => {
-  if (n === 1 || n === 2) return 1;
-  return fibonacci(n - 1) + fibonacci(n - 2);
+const fibonacciNaive = (n) => {
+  if (n <= 2) return 1;
+  return fibonacciNaive(n - 1) + fibonacciNaive(n - 2);
 };
+
+const fibonacciDynamic = (n, memo = []) => {
+  if (memo[n] !== undefined) return memo[n];
+  if (n <= 2) return 1;
+  let res = fibonacciDynamic(n - 1, memo) + fibonacciDynamic(n - 2, memo);
+  memo[n] = res;
+  return res;
+};
+
+const fib = (n) => {
+  let memo = [undefined, 1, 1];
+  memo[n] = fibonacciDynamic(n - 1) + fibonacciDynamic(n - 2);
+  return memo[n];
+};
+
+console.log(fib(333));
