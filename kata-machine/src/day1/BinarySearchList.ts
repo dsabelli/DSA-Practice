e803ed21b65aea2e5db64bceb0e1c73a88468a11
dsabelli <dsabelli3@gmail.com>
2022-09-26 13:28:56 -0400

    binary search

--- kata-machine/src/day1/BinarySearchList.ts
+++ kata-machine/src/day1/BinarySearchList.ts
@@ -1,3 +1,11 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let low: number = 0;
+    let high: number = haystack.length;
+    while (low < high) {
+        let mid: number = Math.floor(low + (high - low) / 2);
+        if (haystack[mid] === needle) return true;
+        else if (haystack[mid] > needle) high = mid;
+        else if (haystack[mid] < needle) low = mid + 1;
+    }
+    return false;
 }
