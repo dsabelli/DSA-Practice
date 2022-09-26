42a4d4c0eb723193a6175ce9b82e77f0cd5d397b
dsabelli <dsabelli3@gmail.com>
2022-09-26 13:29:04 -0400

    sqrt search

--- kata-machine/src/day1/TwoCrystalBalls.ts
+++ kata-machine/src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,15 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const n = Math.floor(Math.sqrt(breaks.length));
+    let broken = 0;
+    for (let i = n; i < breaks.length; i += n) {
+        if (breaks[i]) {
+            broken = i;
+            break;
+        }
+    }
 
+    for (let j = broken - n; j < broken; j++) {
+        if (breaks[j]) return j;
+    }
+    return -1;
 }
