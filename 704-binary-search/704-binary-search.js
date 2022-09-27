a80e4f69252da0243c3fab4c3a5d65c9e8e67b8c
dsabelli <81635950+dsabelli@users.noreply.github.com>
2022-09-27 08:23:54 -0400

    Time: 101 ms (50.40%), Space: 46 MB (6.09%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -6,8 +6,9 @@
 var search = function(nums, target) {
     let low = 0
     let high = nums.length
+    let mid
     while(low<high){
-        let mid = Math.floor((high+low)/2)
+         mid = Math.floor((high+low)/2)
         if (nums[mid]===target) return [mid]
         nums[mid]>target?high=mid:low=mid+1
     }
