d23ab6bc198c52ed00da868d843b16db510872eb
dsabelli <81635950+dsabelli@users.noreply.github.com>
2022-09-27 08:13:16 -0400

    Time: 122 ms (13.72%), Space: 45.5 MB (14.54%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,5 +4,12 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    return nums.findIndex((num)=>num===target)
+    let low = 0
+    let high = nums.length
+    while(low<high){
+        let mid = Math.floor(low+(high-low)/2)
+        if (nums[mid]===target) return [mid]
+        nums[mid]>target?high=mid:low=mid+1
+    }
+    return -1
 };
