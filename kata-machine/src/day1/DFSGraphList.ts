791416fb4a1d92024a01479af756f8084313ca63
dsabelli <dsabelli3@gmail.com>
2022-09-29 10:56:55 -0400

    dfs adj list

--- kata-machine/src/day1/DFSGraphList.ts
+++ kata-machine/src/day1/DFSGraphList.ts
@@ -1,3 +1,39 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+import { AstPath } from "prettier";
 
+function traverse(
+    graph: WeightedAdjacencyList,
+    current: number,
+    needle: number,
+    seen: boolean[],
+    path: number[],
+): boolean {
+    if (seen[current]) return false;
+
+    seen[current] = true;
+
+    path.push(current);
+    if (current === needle) return true;
+
+    const list = graph[current];
+    for (let i = 0; i < list.length; i++) {
+        let edge = list[i];
+        if (traverse(graph, edge.to, needle, seen, path)) {
+            return true;
+        }
+    }
+
+    path.pop();
+    return false;
+}
+
+export default function dfs(
+    graph: WeightedAdjacencyList,
+    source: number,
+    needle: number,
+): number[] | null {
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const path: number[] = [];
+    traverse(graph, source, needle, seen, path);
+    if (path.length === 0) return null;
+    return path;
 }
