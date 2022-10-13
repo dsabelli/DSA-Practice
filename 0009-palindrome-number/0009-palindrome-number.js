723541f3e48db37491376b1806897817bc2930af
dsabelli <81635950+dsabelli@users.noreply.github.com>
2022-10-13 16:39:22 -0400

    Time: 310 ms (49.19%), Space: 51.2 MB (50.75%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,6 +3,14 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if (+x.toString().split("").reverse().join("")===x)return true
-    return false
+const s = x.toString()
+    let p1=0
+    let p2=s.length-1
+    
+    while(p1<p2){
+        if (s[p1]!==s[p2])return false
+        p1++
+        p2--
+    }
+    return true
 };
